ffe6efb6e721060a8409393b77cfd758598463ed
Alisa Chan <alisachan.ny@gmail.com>
2020-08-28 20:49:23 -0400

    added SearchBar component and header

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from "../logo.svg";
+import "./App.css";
+import "./SearchBar";
+import SearchBar from "./SearchBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +20,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <h1>The Shoppies</h1>
+      <SearchBar />
     </div>
   );
 }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SearchBar = () => {
+  return (
+    <form>
+      <label>
+        Movie title
+        <input type="text" />
+      </label>
+    </form>
+  );
+};
+
+export default SearchBar;
