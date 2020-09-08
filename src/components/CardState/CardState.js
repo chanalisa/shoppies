25e4e8117c186cb4e258693fb1d2898f8c6e7edc
Alisa Chan <alisachan.ny@gmail.com>
2020-09-08 01:47:22 -0400

    removed unused coe

--- src/components/CardState/CardState.js
+++ src/components/CardState/CardState.js
@@ -5,14 +5,10 @@ import "./CardState.scss";
 const CardState = (props) => {
   return (
     <div className="CardState">
-      {/* {props.details === "" ? ( */}
       <div>
         <h2 className="card-heading">{props.name}</h2>
         <p className="card-info">{props.detail}</p>
       </div>
-      {/* ) : (
-        <h2 className="card-heading">{props.details}</h2>
-      )} */}
     </div>
   );
 };
